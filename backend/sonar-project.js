c714ef18bca2bf8722862f6292e029214c221615
amarjeetchaurasiya27-cyber <amarjeet.chaurasiya27@gmail.com>
2026-04-29 21:43:07 +0530

    Update sonar-project.js

--- backend/sonar-project.js
+++ backend/sonar-project.js
@@ -2,17 +2,17 @@ const sonarqubeScanner = require('sonarqube-scanner').default;
 
 sonarqubeScanner(
   {
-    serverUrl: 'http://host.docker.internal:9000',
+    // Yahan localhost use karna sabse safe hai agar SonarQube usi PC par hai
+    serverUrl: 'http://localhost:9000', 
     options: {
       'sonar.projectKey': 'amarjeet-node-project',
       'sonar.projectName': 'Amarjeet Node Project',
       'sonar.sources': '.',
-      // Token ko login ki jagah daalna hi sabse bada secret hai
-      'sonar.login': 'squ_3ab001afad8417203a6b35f7eee4d431386ec209', 
+      'sonar.token': 'squ_3ab001afad8417203a6b35f7eee4d431386ec209', // 'login' ki jagah 'token' use karein
     },
   },
   () => {
-    console.log('--- Scan Finished ---');
+    console.log('--- Backend Scan Finished ---');
     process.exit();
   }
 );
