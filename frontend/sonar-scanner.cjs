3534b2f663566a4250889d7d9f5eb8f127d8f7c2
amarjeetchaurasiya27-cyber <amarjeet.chaurasiya27@gmail.com>
2026-04-29 21:44:08 +0530

    Update sonar-scanner.cjs

--- frontend/sonar-scanner.cjs
+++ frontend/sonar-scanner.cjs
@@ -2,13 +2,13 @@ const sonarqubeScanner = require('sonarqube-scanner').default;
 
 sonarqubeScanner(
   {
-    serverUrl: 'http://host.docker.internal:9000',
+    serverUrl: 'http://localhost:9000',
     options: {
       'sonar.projectKey': 'amarjeet-react-frontend',
       'sonar.projectName': 'Amarjeet React Frontend',
       'sonar.projectDescription': 'Frontend quality check for Amarjeet React App',
-      'sonar.sources': 'src',         // React ka asli code 'src' mein hota hai
-      'sonar.login': 'squ_3ab001afad8417203a6b35f7eee4d431386ec209', // Aapka wahi Master Token
+      'sonar.sources': 'src',
+      'sonar.token': 'squ_3ab001afad8417203a6b35f7eee4d431386ec209', // Master Token
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
       'sonar.sourceEncoding': 'UTF-8',
     },
